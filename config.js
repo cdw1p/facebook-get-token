8c55557b1a31f2442eeb227809b253bfb576232b
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-08-26 15:10:32 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+let credentials = {
+  facebook: {
+    username: '',
+    password: ''
+  },
+  sgb: {
+    code: '',
+    secret: ''
+  },
+  browser: `C:/Program Files (x86)/Google/Chrome/Application/chrome.exe`
+}
+
+module.exports = {
+  credentials
+}
